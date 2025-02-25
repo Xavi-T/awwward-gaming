95f37100e568d2076941e345d73f4ca7652ffa3e
sontran <sontranvan@vnpt.vn>
2025-02-25 16:03:56 +0700

    add features section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import About from './components/About';
+import Features from './components/Features';
 import Hero from './components/Hero';
+import Navbar from './components/Navbar';
 import './index.css';
 const App = () => {
   return (
-    <main className="relative min-h-screen w-screen overflow-x-hidden">
+    <main className="relative min-h-screen w-screen overflow-x-hidden bg-zinc-600">
+      <Navbar />
       <Hero />
       <About />
+      <Features />
     </main>
   );
 };
